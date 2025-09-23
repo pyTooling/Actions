91736df13e37069f3b309fc2880295b71e2b8dde
Patrick Lehmann <Paebbels@gmail.com>
2025-09-23 22:54:46 +0200

    Try to package myPackage or myFramework.Extension with same setup-routine.

--- setup.py
+++ setup.py
@@ -28,21 +28,22 @@
 # SPDX-License-Identifier: Apache-2.0                                                                                  #
 # ==================================================================================================================== #
 #
-"""Package installer for 'myPackage'."""
+"""Package installer for 'myPackage' or 'myFramework.Extension'."""
+from os                  import environ
+from pathlib             import Path
 from setuptools          import setup
 
-from pathlib             import Path
 from pyTooling.Packaging import DescribePythonPackageHostedOnGitHub
 
 gitHubNamespace =        "pyTooling"
-packageName =            "myPackage"
-packageDirectory =       packageName
+packageName =            "myPackage" if "." not in environ["artifact"] else "myFramework.Extension"
+packageDirectory =       packageName.replace(".", "/")
 packageInformationFile = Path(f"{packageDirectory}/__init__.py")
 
 setup(
 	**DescribePythonPackageHostedOnGitHub(
 		packageName=packageName,
-		description="myPackage is a test package to verify GitHub actions for Python projects.",
+		description=f"{packageName} is a test package to verify GitHub actions for Python projects.",
 		gitHubNamespace=gitHubNamespace,
 		unittestRequirementsFile=Path("tests/unit/requirements.txt"),
 		sourceFileWithVersion=packageInformationFile,
